a2debc8c8535ce695e71bd6b502ca85b6d26e5d8
Gysen Bart <bart-gysen@hotmail.com>
2025-01-26 00:13:49 +0100

    adde some files

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+#sync_folder = "PATHVARIABLE"
+#dest_folder = "PATHVARIABLE"
+
+sync_folder = os.path.join(basedir, "files/Camera")
+dest_folder = os.path.join(basedir, "files/Photos")
+
